0652acba9641f01c648f4f9d71c9e1768fc1e15d
balasubramanian-rengasamy <vrbala@gmail.com>
2020-11-03 17:05:49 -0500

    first commit

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>{{ message }}</h1>
+    <h2>{{name}} </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Using Parcel In A Vue.js App',
+      name: 'Andy'
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .container {
+    width: 600px;
+    margin: 50px auto;
+    text-align: center;
+  }
+</style>
---
+++ README.md
@@ -0,0 +1,9 @@
+# parcel-vuejs
+
+npm install
+
+npm install -g parcel-bundler
+
+parcel build index.html
+
+browser-sync
